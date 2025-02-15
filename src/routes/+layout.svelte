7e7b7f5ef0f11714672279a81114e0b8429a017a
gabrielatwell1987 <gabrielatwell@proton.me>
2025-02-15 14:53:45 -0800

    added Github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script>
 	import '../app.css';
+	import Github from '$lib/components/Github.svelte';
 
 	let { children } = $props();
 </script>
 
 <main>
 	{@render children()}
+
+	<Github />
 </main>
 
 <style>
